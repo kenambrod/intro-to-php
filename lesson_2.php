a2d1dac51470acb15037a3f75e4943a942b0ed98
kenambrod <kenambrod@gmail.com>
2019-11-12 11:26:28 +0300

    lesson2

--- lesson_2.php
+++ lesson_2.php
@@ -19,5 +19,34 @@ echo $y;
 //joining a number and a string
 
 echo"iam $y years old";
+echo"<br>";
 echo "iam " . $y . "years old";
+echo"<br>";
 //dot operator is used to join interger and string or variable and variable
+
+
+//php DATATYPES
+//1.STRING
+//2.INTERGER
+//3.FLOAT(decimal_point)
+//4.BOOLEAN (state true or false)
+
+
+//STRINGS
+//A function is a block of code that does something specific
+//function in php are written as : functionname()
+//strlen()  counts number of characters in a string
+
+echo strlen("hello world, hello");
+echo"<br>";
+
+$name = "john";
+echo strlen($name);
+echo"<br>";
+
+//assignment: research on the following string function
+//str_word_count
+//strrev()
+//strpos()
+//str_replace()
+//research and implement on the following and implement the string functions
