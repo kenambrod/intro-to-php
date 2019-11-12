1fa739d50314e5b849f8ee66de1071fbf4a672bb
kenambrod <kenambrod@gmail.com>
2019-11-12 10:51:04 +0300

    additional lesson

---
+++ lesson_2.php
@@ -0,0 +1,23 @@
+<?php
+//echo //basic ways in php to output data returns a value
+//print//basic way of outputing data bt doesnot return a value
+
+//every php statement ends with a semicolon
+
+//output html code
+//pathfile starts from localhost/folder/file
+echo"<p> hello welcome</p>";
+echo "hello world am a php string";
+// a string is a text/sequence of characters inside "" or'' quotes
+//interger
+echo 200;
+
+$x = 12;
+$y = 20;
+echo $x;
+echo $y;
+//joining a number and a string
+
+echo"iam $y years old";
+echo "iam " . $y . "years old";
+//dot operator is used to join interger and string or variable and variable
