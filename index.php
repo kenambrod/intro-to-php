adfc075ac250254443ed94a1c2b9e757d60133d7
kenambrod <kenambrod@gmail.com>
2019-11-12 10:24:44 +0300

    push process

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+//// variables in php rules
+//1.must start with dollar sign
+//2.should start with lower case
+//3.for multi word variable use camecasing($numberOfChildren)
+//    4. can contain alphanumeric characters e.g $number2
+//    5.cannot start with a number e.g $2people.
+// process of pushing to git hub
+//1.create git local repo
+//make sure you are inside the folder that has the code in the terminal
+//$git init
+// $ git add index.php
+//$git add . (space btn add and  .)
+//
