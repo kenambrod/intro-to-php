38c838c13a960addd56e6e7ae813e76de24d0852
kenambrod <kenambrod@gmail.com>
2019-11-15 11:22:07 +0300

    adding on

--- lesson_5.php
+++ lesson_5.php
@@ -18,7 +18,7 @@ while($x < 5){ // checking conditon true first
     echo " $x hello world  <br>";
     $x ++ ; // increment
 }
-
+echoThreeTimes();
 //do while loop checks condition later
 //syntax
 //do{code to execute
@@ -49,3 +49,73 @@ echo"assignment <br>";
 for($c = 13; $c < 34 ;$c++){
     echo "$c <br>";
 }
+
+//FUNCTION- BLOCK of code that performs specific task,WHEN CALLED
+//syntax
+//function nameoffunction(){
+//    
+//}
+
+function echoThreeTimes(){
+    echo "i love coding in php <br>";
+    echo "i am a php geek <br>";
+    echo " coding is awesome<br>";
+}
+
+//calling a function
+
+echoThreeTimes();
+
+function greetings(){
+    echo "hello john <br>";
+}
+
+greetings();
+
+//functions that take arguments
+
+
+//function greetings1($name){
+//    echo "hello $name <br>";
+//}
+//
+//greetings1("john");
+//greetings1("joice");
+//greetings1("lucas");
+//greetings1("arnold");
+
+
+function greetings2($firstName,$lastName,$idNo){
+    $fname =$firstName;
+    $lname = $lastName;
+    $id =$idNo;
+
+     $details = "your details are First name :$fname 
+                 last name :$lname 
+                 ID Number : $id";
+     echo $details;
+}
+
+greetings2("john; paul","paul","31309989");
+
+//RETURN
+function greetings3($firstName,$lastName,$idNo){
+
+    $details = "your details are First name :$firstName
+                 last name :$lastName
+                 ID Number : $idNo";
+
+    return $details;
+}
+
+$userData = greetings3("john","paul","20203030");
+
+echo"$userData <br>";
+
+//functions with default value
+function greetings4( $name = "Developer"){
+    echo "Hello $name <br>";
+}
+
+greetings4("Opiyo");
+greetings4();
